4d7a6c067622959c3971f3763dc807d27986a5ac
IMeitonI <60201796+IMeitonI@users.noreply.github.com>
2020-01-22 23:36:38 -0500

    Add files via upload

---
+++ Tarea 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tarea_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double y = 2;
+			Console.WriteLine("y=" + y);
+			double z = 3;
+			Console.WriteLine("z=" + z);
+			double t = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(z, 2));
+			Console.WriteLine("t=" + t);
+
+			double cAngulo = Math.Asin(z / t);
+			double cAnguloF = cAngulo * (180.0 / Math.PI);
+			Console.WriteLine("Angulo de c: " + cAnguloF);
+
+			double aAngulo = Math.Acos(z / t);
+			double aAngulof = aAngulo * (180.0 / Math.PI);
+			Console.WriteLine("Angulo de a: " + aAngulof);
+
+				 
+		}
+	}
+}
