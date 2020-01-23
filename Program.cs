d123d7b6b65ce096c87939850af9c8198cc80172
IMeitonI <60201796+IMeitonI@users.noreply.github.com>
2020-01-22 23:25:39 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 
-namespace Tarea_1._3_parte
+namespace Tarea_1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double z = 3;
-            Console.WriteLine("z=" + z);
-            double cGrados = 56.31;
-            Console.WriteLine("c=" + cGrados);
-            double c = cGrados * (Math.PI / 180.0);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double y = 2;
+			Console.WriteLine("y=" + y);
+			double z = 3;
+			Console.WriteLine("z=" + z);
+			double t = Math.Sqrt(Math.Pow(y, 2) + Math.Pow(z, 2));
+			Console.WriteLine("t=" + t);
 
-            double y = z / (Math.Tan(c));
-            Console.WriteLine("y=" + y);
+			double cAngulo = Math.Asin(z / t);
+			double cAnguloF = cAngulo * (180.0 / Math.PI);
+			Console.WriteLine("Angulo de c: " + cAnguloF);
 
-            double aGrados = 180 - (90 + cGrados);
-            Console.WriteLine("a=" + aGrados);
+			double aAngulo = Math.Acos(z / t);
+			double aAngulof = aAngulo * (180.0 / Math.PI);
+			Console.WriteLine("Angulo de a: " + aAngulof);
 
-            double t = Math.Sqrt(Math.Pow(y, 2) + (Math.Pow(z, 2)));
-            Console.WriteLine("t=" + t);
-        }
-    }
+				 
+		}
+	}
 }
