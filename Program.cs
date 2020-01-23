4a8933807de75348e408941c3647798eab9c6309
IMeitonI <60201796+IMeitonI@users.noreply.github.com>
2020-01-22 23:26:45 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,25 @@
 ﻿using System;
 
-namespace Tarea_1._2_parte
+namespace Tarea_1._3_parte
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double t = 3.61;
-            Console.WriteLine("t=" + t);
-            double aGrados = 33.69;
-            Console.WriteLine("a=" + aGrados);
-            double a = aGrados * (Math.PI / 180.0);
+            double z = 3;
+            Console.WriteLine("z=" + z);
+            double cGrados = 56.31;
+            Console.WriteLine("c=" + cGrados);
+            double c = cGrados * (Math.PI / 180.0);
 
-            double angulo90 = 90;
-            double c = 180 - (angulo90 + aGrados);
-            Console.WriteLine("c=" + c);
-
-            double y = (Math.Sin(a)) * t;
+            double y = z / (Math.Tan(c));
             Console.WriteLine("y=" + y);
 
-            double z = Math.Sqrt(Math.Pow(t, 2) - Math.Pow(y, 2));
-            Console.WriteLine("z=" + z);
+            double aGrados = 180 - (90 + cGrados);
+            Console.WriteLine("a=" + aGrados);
 
-
+            double t = Math.Sqrt(Math.Pow(y, 2) + (Math.Pow(z, 2)));
+            Console.WriteLine("t=" + t);
         }
     }
 }
