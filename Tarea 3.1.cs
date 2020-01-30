31c62589f4cbab80fb4453a1784684194516c9a7
IMeitonI <60201796+IMeitonI@users.noreply.github.com>
2020-01-29 22:27:35 -0500

    Add files via upload

---
+++ Tarea 3.1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majo_Tarea_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario en SMMLV");
+            int salario = int.Parse(Console.ReadLine());
+
+             if (salario < 2){
+                Console.WriteLine("Tarifa A");
+             }
+              else if (2 <= salario && salario <= 4) 
+              {
+                Console.WriteLine("Tarifa B");
+              }
+              else {
+                Console.WriteLine("Tarifa C");
+              }
+
+
+
+        }
+    }
+}
---
+++ Tarea 3.2 .cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Majo_Tarea_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese su salario en SMMLV");
+            int salario = int.Parse(Console.ReadLine());
+        
+                       
+            if (salario < 2)
+            {
+                Console.WriteLine("Tarifa A");
+                Console.WriteLine("Cuota moderada: $3400" );
+            }
+            else if (2 <= salario && salario <= 5)
+            {
+                Console.WriteLine("Tarifa B");
+                Console.WriteLine("Cuota moderada: $13500");
+            }
+            else
+            {
+                Console.WriteLine("Tarifa C");
+                Console.WriteLine("Cuota moderada: $35600");
+            }
+
+        }
+    }
+}
