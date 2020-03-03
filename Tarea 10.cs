2c1ff287bd5466dc5e193df3000c194bb400ce4a
IMeitonI <60201796+IMeitonI@users.noreply.github.com>
2020-03-03 01:40:35 -0500

    Add files via upload

--- Tarea 10.cs
+++ Tarea 10.cs
@@ -1,67 +1,112 @@
 ﻿using System;
 
-namespace Tarea_11
+namespace Tarea_10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double maximo = 0;
-            double[] cx = { 0, 2, 3, 7 };
-            double[] cy = { 0, 1, 5, 6 };
+            Random aleatorio = new Random();
+            int carta = 0, jugador = 1, max = 0, min = 22, medio = 0;
+            string continuar = "s", nombre = "", nombremayor = "", segundo="";
+            Console.WriteLine("Ingrese número de jugadores (Minimo 2 jugadores y maximo 5)");
+            int n = int.Parse(Console.ReadLine());
+            n++;
+            int[] puntos = new int[n];
 
-            double m1 = (cy[1] - cy[0]) / (cx[1] - cx[0]);
-            double m2 = (cy[2] - cy[0]) / (cx[2] - cx[0]);
-            double m3 = (cy[3] - cy[0]) / (cx[3] - cx[0]);
-            double m4 = (cy[2] - cy[1]) / (cx[2] - cx[1]);
-            double m5 = (cy[3] - cy[2]) / (cx[3] - cx[2]);
-            double m6 = (cy[3] - cy[1]) / (cx[3] - cx[1]);
-
-            if (m1 == m2 && m2 == m3 && m3 == m4 && m4 == m5 && m5 == m6)
+            while (n < 3 || n > 6)
             {
-                Console.WriteLine("pertenecen a una misma recta");
-            }
-            else
-            {
-                Console.WriteLine("No pertenecen a la misma recta");
+                Console.WriteLine("Error. (Minimo 2 jugadores, maximo 5)");
+                n = int.Parse(Console.ReadLine());
+                n++;
             }
 
-            double d1 = Math.Sqrt(Math.Pow((cy[1] - cy[0]), 2) + Math.Pow((cx[1] - cx[0]), 2));
-            double d2 = Math.Sqrt(Math.Pow((cy[2] - cy[0]), 2) + Math.Pow((cx[2] - cx[0]), 2));
-            double d3 = Math.Sqrt(Math.Pow((cy[3] - cy[0]), 2) + Math.Pow((cx[3] - cx[0]), 2));
-            double d4 = Math.Sqrt(Math.Pow((cy[2] - cy[1]), 2) + Math.Pow((cx[2] - cx[1]), 2));
-            double d5 = Math.Sqrt(Math.Pow((cy[3] - cy[2]), 2) + Math.Pow((cx[3] - cx[2]), 2));
-            double d6 = Math.Sqrt(Math.Pow((cy[3] - cy[1]), 2) + Math.Pow((cx[3] - cx[1]), 2));
+            while( jugador < n)
+            {
+                Console.WriteLine("\n\nBienvenido jugador: " + jugador);
+                
+                puntos[jugador]  = 0;
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine();
 
-            if (d1 > maximo)
-            {
-                maximo = d1;
-            }
-            if (d2 > maximo)
-            {
-                maximo = d2;
-            }
-            if (d3 > maximo)
-            {
-                maximo = d3;
-            }
-            if (d4 > maximo)
-            {
-                maximo = d4;
-            }
-            if (d5 > maximo)
-            {
-                maximo = d5;
-            }
-            if (d6 > maximo)
-            {
-                maximo = d6;
-            }
+                while (continuar == "s" && puntos[jugador]  < 21)
+                {
+                    carta = aleatorio.Next(1, 11);
+                    puntos[jugador]  += carta;
+                    Console.WriteLine("carta: " + carta);
+                    carta = aleatorio.Next(1, 11);
+                    puntos[jugador]  += carta;
+                  
+                    Console.WriteLine("carta: " + carta);
+                    Console.WriteLine("Total: " + puntos[jugador] );
+                    Console.WriteLine("Desea tomar otra carta? (s/n)");
+                    continuar = Console.ReadLine();
 
-            Console.WriteLine("La mayor distancia: " + maximo);
+                    while (continuar != "s" && continuar != "n")
+                    {
+                        Console.WriteLine("Error. Ingrese (s/n)");
+                        continuar = Console.ReadLine();
+                    }
 
+                    while (puntos[jugador]  < 21 && continuar == "s")
+                    {
+                        carta = aleatorio.Next(1, 11);
+                        Console.WriteLine("carta: " + carta);
+                        puntos[jugador]  += carta;
+                       
+                        Console.WriteLine("Total: " + puntos[jugador] );
+
+                        if (puntos[jugador]  > max && puntos[jugador]  <= 21)
+                        {
+                            max = puntos[jugador] ;
+                            nombremayor = nombre;
+                        }
+                        if (puntos[jugador]  < min && puntos[jugador]  <= 21)
+                        {
+                            min = puntos[jugador] ;
+                        }
+                        if (puntos[jugador]  < max && puntos[jugador]  > min)
+                        {
+                            medio = puntos[jugador] ;
+                            segundo = nombre;
+                        }
+
+                        if (puntos[jugador]  > 21)
+                        {
+                            Console.WriteLine("Has perdido.");
+                            continuar = "n";
+                            break;
+                        }
+
+                        else if (puntos[jugador]  == 21)
+                        {
+                            Console.WriteLine("Ganaste.");
+                            continuar = "n";
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Desea tomar otra carta? (s/n)");
+                            continuar = Console.ReadLine();
+                        }
+
+                        while (continuar != "s" && continuar != "n")
+                        {
+                            Console.WriteLine("Error. Ingrese (s/n)");
+                            continuar = Console.ReadLine();
+                        }
+                    }
+                }
+                Console.WriteLine("Gracias por participar");
+                jugador++;
+                continuar = "s";
+
+            }
+            Console.WriteLine("Ganador: " + nombremayor + " Segundo lugar: "+ segundo);
+            for(int j =1; j <n; j++)
+            {
+                Console.WriteLine("punto " +j +" "+ puntos[j]);
+            }
         }
     }
 }
-      
-    
